950e43eafec023ec19d5b734d781077dc0aef706
Angelina Patel <angelinapatel@Angelinas-MBP.attlocal.net>
2020-11-21 01:00:18 -0500

    autocomplete

---
+++ autocomplete.js
@@ -0,0 +1,93 @@
+function autocomplete(inp, arr) {
+    var currentFocus;
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+          if (arr[i].toUpperCase().indexOf(val.toUpperCase()) > -1) {
+            b = document.createElement("DIV");
+            b.innerHTML += arr[i];
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            b.addEventListener("click", function(e) {
+                inp.value = this.getElementsByTagName("input")[0].value;
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+
+        window.onclick = function(e) {
+          window.location = `team.html#${$(e.target).text()}`;
+          addActive(x);
+        }
+        if (e.keyCode == 40) {
+          currentFocus++;
+          addActive(x);
+        } else if (e.keyCode == 38) {
+          currentFocus--;
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          window.location = `team.html#${$(e.target).text()}`;
+          addActive(x);
+          e.preventDefault();
+          if (currentFocus > -1) {
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+
+    function addActive(x) {
+      if (!x) return false;
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode.removeChild(x[i]);
+        }
+      }
+    }
+
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+  }
+  
+  var places = [
+    'Davis',
+    'Undergraduate Library',
+    'The Quad',
+    'The Pit',
+    'Bottom of Lenoir',
+    'Genome Sciences Cafe',
+    'Sitterson Lobby',
+    'Sloane Art Library',
+    'Phillips Hall',
+    'TOPO',
+    'Starbucks on Franklin',
+    'Kenan Science Library',
+    'Wilson Library',
+    'Student Union',
+  ]
+
+  autocomplete(document.getElementById("theInput"), places);
