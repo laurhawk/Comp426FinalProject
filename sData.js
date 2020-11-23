37366c9fe9216ce5b63d3daeaabf13746260f405
Lara Kirby <laraek@live.unc.edu>
2020-11-23 13:07:48 -0500

    starting data

--- sData.js
+++ sData.js
@@ -1,113 +1,113 @@
 const spotData = [{
   "id": 1,
   "name": "Davis Library",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true],
+  "ratings": [4],
+  "comments": ["Large"],
   "image": "../images/davis.jpg"
 },
 {
   "id": 2,
   "name": "Undergraduate Library",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true, true],
+  "ratings": [3.5, 4],
+  "comments": ["Scary at night"],
   "image": "../images/ul.jpg"
 },
 {
   "id": 3,
   "name": "The Quad",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [3],
+  "comments": ["Busy, has insects"],
   "image": "../images/quad.jpg"
 },
 {
   "id": 4,
   "name": "The Pit",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [2],
+  "comments": ["Crowded and loud"],
   "image": "../images/pit.jpg"
 },
 {
   "id": 5,
   "name": "Bottom of Lenoir",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true],
+  "ratings": [5],
+  "comments": ["Never busy!"],
   "image": "../images/lenoir.jpg"
 },
 {
   "id": 6,
   "name": "Genome Sciences Cafe",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true],
+  "ratings": [5],
+  "comments": ["Close to lecture halls"],
   "image": "../images/genome.jpg"
 },
 {
   "id": 7,
   "name": "Sitterson Lobby",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true],
+  "ratings": [2],
+  "comments": ["Just convenient"],
   "image": "../images/sitterson.jpg"
 },
 {
   "id": 8,
   "name": "Sloane Art Library",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [3],
+  "comments": ["Never has seating"],
   "image": "../images/sloane.jpg"
 },
 {
   "id": 9,
   "name": "Phillips Hall",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [1],
+  "comments": ["Terrifying"],
   "image": "../images/phillips.jpg"
 },
 {
   "id": 10,
   "name": "TOPO",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [1],
+  "comments": ["Requires purchase"],
   "image": "../images/topo.jpg"
 },
 {
   "id": 11,
   "name": "Starbucks on Franklin",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [false],
+  "ratings": [2],
+  "comments": ["Always hot"],
   "image": "../images/starbucks.jpg"
 },
 {
   "id": 12,
   "name": "Kenan Science Library",
-  "wouldStudy": [true],
-  "ratings": [4],
+  "wouldStudy": [true, false],
+  "ratings": [4, 2],
   "comments": ["Great, quiet place!"],
   "image": "../images/kenan.jpg"
 },
 {
   "id": 13,
   "name": "Wilson Library",
-  "wouldStudy": [true],
-  "ratings": [3.5],
+  "wouldStudy": [true, false],
+  "ratings": [3.5, 3],
   "comments": ["Too quiet"],
   "image": "../images/wilson.jpg"
 },
 {
   "id":14,
   "name": "Student Union",
-  "wouldStudy": [],
-  "ratings": [],
-  "comments": [],
+  "wouldStudy": [true],
+  "ratings": [3],
+  "comments": ["Uncomfortable chairs"],
   "image": "../images/union.jpg"
 }
 ]
