1d21b4e38a6f4099bb55354942c677e63c989148
Angelina Patel <angelinapatel@Angelinas-MacBook-Pro.local>
2020-11-23 22:37:06 -0500

    cleaned up autocomplete code: nit

--- autocomplete.js
+++ autocomplete.js
@@ -1,68 +1,65 @@
+let places = [
+  'Davis',
+  'Undergraduate Library',
+  'The Quad',
+  'The Pit',
+  'Bottom of Lenoir',
+  'Genome Sciences Cafe',
+  'Sitterson Lobby',
+  'Sloane Art Library',
+  'Phillips Hall',
+  'TOPO',
+  'Starbucks on Franklin',
+  'Kenan Science Library',
+  'Wilson Library',
+  'Student Union',
+]
+
 function autocomplete(inp, arr) {
-    var currentFocus;
-    inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        this.parentNode.appendChild(a);
-        for (i = 0; i < arr.length; i++) {
-          if (arr[i].toUpperCase().indexOf(val.toUpperCase()) > -1) {
-            b = document.createElement("DIV");
-            b.innerHTML += arr[i];
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            b.addEventListener("click", function(e) {
-                inp.value = this.getElementsByTagName("input")[0].value;
-                closeAllLists();
-            });
-            a.appendChild(b);
-          }
-        }
-    });
-    function addActive(x) {
-      if (!x) return false;
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-      }
-    }
-    function closeAllLists(elmnt) {
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-          x[i].parentNode.removeChild(x[i]);
-        }
-      }
-    }
+  let currentItem;
+  inp.addEventListener("input", function (e) {
+    let x, y, z, val = this.value;
+    closeAllLists();
 
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
+    if (!val) { return false; }
+
+    currentItem = -1;
+
+    x = document.createElement("DIV");
+    x.setAttribute("id", this.id + "autocomplete-places");
+    x.setAttribute("class", "autocomplete-spots");
+
+    this.parentNode.appendChild(x);
+
+    for (z = 0; z < arr.length; z++) {
+      if (arr[z].toUpperCase().indexOf(val.toUpperCase()) > -1) {
+
+        y = document.createElement("DIV");
+        y.innerHTML += arr[z];
+        y.innerHTML += "<input type='hidden' value='" + arr[z] + "'>";
+
+        y.addEventListener("click", function (e) {
+          inp.value = this.getElementsByTagName("input")[0].value;
+          closeAllLists();
+        });
+        x.appendChild(y);
+      }
+    }
+  });
+
+  function closeAllLists(items) {
+
+    let x = document.getElementsByClassName("autocomplete-spots");
+    for (let i = 0; i < x.length; i++) {
+      if (items != x[i] && items != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
   }
-  
-  var places = [
-    'Davis',
-    'Undergraduate Library',
-    'The Quad',
-    'The Pit',
-    'Bottom of Lenoir',
-    'Genome Sciences Cafe',
-    'Sitterson Lobby',
-    'Sloane Art Library',
-    'Phillips Hall',
-    'TOPO',
-    'Starbucks on Franklin',
-    'Kenan Science Library',
-    'Wilson Library',
-    'Student Union',
-  ]
 
-  autocomplete(document.getElementById("theInput"), places);
+  document.addEventListener("click", function (clicker) {
+    closeAllLists(clicker.target);
+  });
+}
+
+autocomplete(document.getElementById("theInput"), places);
